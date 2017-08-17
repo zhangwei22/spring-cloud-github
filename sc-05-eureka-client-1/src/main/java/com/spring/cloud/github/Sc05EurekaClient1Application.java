a20b83258777bd87e44b7cc2c6705bf1040bcee3
zhangwei <zhangwei@mimidai.com>
2017-08-17 12:05:09 +0800

    hystrix dashboard

--- sc-05-eureka-client-1/src/main/java/com/spring/cloud/github/Sc05EurekaClient1Application.java
+++ sc-05-eureka-client-1/src/main/java/com/spring/cloud/github/Sc05EurekaClient1Application.java
@@ -2,6 +2,7 @@ package com.spring.cloud.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
