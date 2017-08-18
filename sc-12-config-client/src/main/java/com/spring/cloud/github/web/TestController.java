e3b0dedbfc9ef81e6dbbb5bd424b825d10a1bd0c
zhangwei <zhangwei@mimidai.com>
2017-08-18 10:33:29 +0800

    config repository version control

--- sc-12-config-client/src/main/java/com/spring/cloud/github/web/TestController.java
+++ sc-12-config-client/src/main/java/com/spring/cloud/github/web/TestController.java
@@ -1,13 +1,24 @@
 package com.spring.cloud.github.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by zhangwei on 17/8/18.
  */
-//@EnableAutoConfiguration
-//@RefreshScope
 @RestController
+@RefreshScope
+@RequestMapping(value = "test")
 public class TestController {
 
+    @Value("${from:spring cloud config!}")
+    private String name;
+
+    @RequestMapping(value = "home", method = RequestMethod.GET)
+    public String home() {
+        return "Hello " + name;
+    }
 }
